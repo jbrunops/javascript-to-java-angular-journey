a4f543d8449d1c9dc115452e29d63a6635f6b675
Jackson Porciúncula <jbrunops@outlook.com>
2025-07-29 15:15:49 -0300

    Exercício finalizado!

--- aula6.js
+++ aula6.js
@@ -1,17 +1,35 @@
-console.log("--- aula 6 - Desestruturação ---");
+// console.log("--- aula 6 - Desestruturação ---");
 
-const filialCentro = {
-  nome: "Pizza Central",
-  faturamento: 5500,
-  ativa: true,
-  endereco: {
-    rua: "Rua das Flores",
-    numero: 123,
-    cidade: "São Paulo"
+// const filialCentro = {
+//   nome: "Pizza Central",
+//   faturamento: 5500,
+//   ativa: true,
+//   endereco: {
+//     rua: "Rua das Flores",
+//     numero: 123,
+//     cidade: "São Paulo"
+//   }
+// };
+
+// const { nome, faturamento, endereco: { rua, cidade } } = filialCentro;
+
+// console.log(`A filial ${nome}, tem um faturamento de ${faturamento}!`);
+// console.log(`A filial ${nome} fica na rua ${rua}, na cidade ${cidade}!`);
+
+console.log("--- exercício de desestruturação ---");
+
+const pedido = {
+  id: 1234,
+  nomeCLiente: "João Silva",
+  endereco: "Rua da Paz, 42",
+  valorTotal: 89.50,
+  pagamento: {
+    tipo: "Cartão de Crédito",
+    bandeira: "Visa"
   }
 };
 
-const { nome, faturamento, endereco: { rua, cidade } } = filialCentro;
+const { nomeCLiente, valorTotal } = pedido;
+const { pagamento: { tipo } } = pedido;
 
-console.log(`A filial ${nome}, tem um faturamento de ${faturamento}!`);
-console.log(`A filial ${nome} fica na rua ${rua}, na cidade ${cidade}!`);
+console.log(`O cliente ${nomeCLiente} fez um pedido do R$ ${valorTotal} e pagará com ${tipo}`);
