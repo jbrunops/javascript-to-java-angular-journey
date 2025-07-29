aae70dbca72128fc13143c75cbadadebb5208728
Jackson Porciúncula <jbrunops@outlook.com>
2025-07-29 13:26:49 -0300

    Operadores finalizados - exemplos simples para estudo

--- condicionais-&&.js
+++ condicionais-&&.js
@@ -1,6 +1,35 @@
-let estoqueDePizza = 20;
+console.log("--- gerente durão ---")
+
+// &&
+
+let estoqueDePizza = 10;
 let estoqueDeRefri = 25;
 
 if(estoqueDePizza > 0 && estoqueDeRefri > 0){
-  console.log("Ótimo! Estamos prontos para abrir o negócio!")
-}
+  console.log("Ótimo! Estamos prontos para abrir o negócio!");
+}else{
+  console.log("Promoção Combo pausada.");
+};
+
+console.log("--- gerente flexivel ---");
+
+// ||
+
+const valorDoPedido = 80.00;
+const ehClienteVIP = true;
+
+if(valorDoPedido > 100 || ehClienteVIP === true){
+  console.log("Entrega grátis liberada para este pedido!");
+}else{
+  console.log("Cobrar taxa de entrega de R$5,00.");
+};
+
+console.log("--- gerente do contra ---");
+
+// ! (NÃO /NOT)
+
+const maquinaCartaoFunciona = false;
+
+if(!maquinaCartaoFunciona){
+  console.log("AVISO: Aceitar apenas pagamento em dinheiro ou PIX!");
+};
