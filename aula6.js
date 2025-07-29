77d8b100d053eb8836d08f197fa392527b3974ce
Jackson Porciúncula <jbrunops@outlook.com>
2025-07-29 15:26:44 -0300

    Usando Spread em Arrays - Combinando listas

--- aula6.js
+++ aula6.js
@@ -16,20 +16,45 @@
 // console.log(`A filial ${nome}, tem um faturamento de ${faturamento}!`);
 // console.log(`A filial ${nome} fica na rua ${rua}, na cidade ${cidade}!`);
 
-console.log("--- exercício de desestruturação ---");
+// console.log("--- exercício de desestruturação ---");
 
-const pedido = {
-  id: 1234,
-  nomeCLiente: "João Silva",
-  endereco: "Rua da Paz, 42",
-  valorTotal: 89.50,
-  pagamento: {
-    tipo: "Cartão de Crédito",
-    bandeira: "Visa"
-  }
+// const pedido = {
+//   id: 1234,
+//   nomeCLiente: "João Silva",
+//   endereco: "Rua da Paz, 42",
+//   valorTotal: 89.50,
+//   pagamento: {
+//     tipo: "Cartão de Crédito",
+//     bandeira: "Visa"
+//   }
+// };
+
+// const { nomeCLiente, valorTotal } = pedido;
+// const { pagamento: { tipo } } = pedido;
+
+// console.log(`O cliente ${nomeCLiente} fez um pedido do R$ ${valorTotal} e pagará com ${tipo}`);
+
+console.log("--- O Operador (Spread e Rest) ---");
+
+const fichaClienteBasica = {
+  nome: "Juliana",
+  telefone: "9999-8888"
 };
 
-const { nomeCLiente, valorTotal } = pedido;
-const { pagamento: { tipo } } = pedido;
+const pedidoCliente = {
+  ...fichaClienteBasica,
+  idPedido: 465,
+  pizza: "Calabresa",
+  valor: 55.50
+};
 
-console.log(`O cliente ${nomeCLiente} fez um pedido do R$ ${valorTotal} e pagará com ${tipo}`);
+console.log(pedidoCliente);
+
+console.log("--- usando em arrays ---");
+
+const bebidasSemAlcool = ["Coca-cola", "Guaraná", "Água"];
+const bebidasAlcoolicas = ["Cerveja", "Vinho"];
+
+const cardapioBebidasCompleto = [...bebidasAlcoolicas, ...bebidasSemAlcool, "Suco de Laranja"];
+
+console.log(cardapioBebidasCompleto);
