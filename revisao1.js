6d1f4d08e31b5ace402c34388c1f694358c54f6e
Jackson Porciúncula <jbrunops@outlook.com>
2025-07-30 15:09:37 -0300

    Revisando Operador Spread e o Operador Rest (...)

--- revisao1.js
+++ revisao1.js
@@ -1,1 +1,12 @@
-// revisão 1 geral
+const trabalharPizzaria = (idade, indicacao) => {
+  if (idade >= 18 && indicacao){
+    console.log("Ótimo, você pode trabalhar com a gente!");
+  }else if (idade >= 18 || indicacao){
+    console.log("Você não tem idade, mas acredito que possa trabalhar pela indica~çao!");
+  }else{
+    console.log("Nada feito!");
+  };
+};
+
+trabalharPizzaria(19, true);
+trabalharPizzaria(15, false);
