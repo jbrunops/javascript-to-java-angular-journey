821d2f621034d295984a98fd26252e16750879c4
Jackson Porciúncula <jbrunops@outlook.com>
2025-07-30 20:38:21 -0300

    Agora em Arrays

--- aula8.js
+++ aula8.js
@@ -1,16 +1,37 @@
-const pizzaDeQueijo = {
-  molho: "Tomate",
-  queijo: "Mussarela"
-};
+// const pizzaDeQueijo = {
+//   molho: "Tomate",
+//   queijo: "Mussarela"
+// };
 
-const pizzaDeNutella = {
-  base: "Chocolate",
-  calda: "Morando"
-};
+// const pizzaDeNutella = {
+//   base: "Chocolate",
+//   calda: "Morando"
+// };
 
-const pedidoDoCLiente = {
-  ...pizzaDeQueijo,
-  delivery: 2.50,
-};
+// const pedidoDoCLiente = {
+//   ...pizzaDeQueijo,
+//   delivery: 2.50,
+// };
 
-console.log(pedidoDoCLiente);
+// console.log(pedidoDoCLiente);
+
+console.log("--- Spread em Arrays ---");
+
+const refrigerantes = [
+  "coca-cola",
+  "guaraná"
+];
+
+const sucos = [
+  "laranja",
+  "abacaxi"
+];
+
+const cardapioDeBebidas = [
+  ...refrigerantes,
+  ...sucos,
+  "Água Mineral"
+];
+
+console.log("--- RESULTADO ---");
+console.log(cardapioDeBebidas);
