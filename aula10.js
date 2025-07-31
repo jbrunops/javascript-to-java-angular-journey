b6fed30422a22aab65ece1ecb034db38b284c9c2
Jackson Porciúncula <jbrunops@outlook.com>
2025-07-31 11:08:41 -0300

    Entendimento 100% do find, mostrando alguns exemplos importantes

--- aula10.js
+++ aula10.js
@@ -7,12 +7,34 @@ const pedidosDaPizzaria = [
   { id: 6, nome: "Cícero", valor: 1022, divida: 233}
 ];
 
+const descobrirIdEspecifico = pedidosDaPizzaria.find((selecionar) => {
+  return selecionar.id === 5;
+
+  // if (selecionar.id === 5){
+  //   console.log(`O ID que estávamos procurando era ${selecionar.id}`);
+  // }else{
+  //   console.log("Não enconstramos o que estávamos procurando!")
+  // }
+});
+
+console.log(descobrirIdEspecifico);
+
+// const verificarDivida = pedidosDaPizzaria.filter((verificar) => {
+//   if (verificar.divida > 0){
+//     console.log(`Os individados são: ${verificar.nome}`);
+//   }else{
+//     console.log("Ninguém está devendo nada.")
+//   }
+// });
+
+// console.log(verificarDivida);
+
 // const listaParaEntregadorCobrar = pedidosDaPizzaria.map((cobrar) => {
 //   return cobrar.valor;
 // });
 
 // forma mais curta!
 
-const listaParaEntregadorCobrar = pedidosDaPizzaria.map(cobrar => cobrar.valor);
+// const listaParaEntregadorCobrar = pedidosDaPizzaria.map(cobrar => cobrar.valor);
 
-console.log(listaParaEntregadorCobrar);
+// console.log(listaParaEntregadorCobrar);
