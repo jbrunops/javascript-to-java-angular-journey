be8c926044c8c397a4b9fe07ad667859230e113b
Jackson Porciúncula <jbrunops@outlook.com>
2025-07-30 21:24:45 -0300

    Pegadinha Callback! ESTUDAR MELHOR PARA ENTENDER A FUNDO!

--- aula9.js
+++ aula9.js
@@ -11,10 +11,23 @@ const pedidos = [
 // console.log("--- USANDO MAP ---");
 // console.log(listaDePizzas);
 
-console.log("--- USANDO FILTER ---");
+// console.log("--- USANDO FILTER ---");
 
-const pedidosCaros = pedidos.filter((pedido) => {
-  return pedido.valor > 55;
-})
+// const pedidosCaros = pedidos.filter((pedido) => {
+//   return pedido.valor > 40;
+// })
 
-console.log(pedidosCaros);
+// console.log(pedidosCaros);
+
+console.log("--- USANDO O FIND ---");
+const pedidoDoBruno = pedidos.find((pedido) =>{
+  if (pedido.id === 11){
+    return "Ótimo! Você escolheu 11";
+  }else if (pedido.id === 13){
+    return "Legal, você escolheu 13";
+  }else{
+    return "Nenhum dos números escolhidos!";
+  }
+});
+
+console.log(pedidoDoBruno);
