49c3b7ecd20d995501649aacfff251ea5926d9c2
Jackson Porciúncula <jbrunops@outlook.com>
2025-07-31 22:32:12 -0300

    Usando um único loop para ser mais eficiente (callback)

--- erros.js
+++ erros.js
@@ -1,22 +1,33 @@
-const pedidosDaPizzaria = [
-  { id: 1, nome: "Caio", divida: 0, saldo: 0 },
-  { id: 2, nome: "Lucas", divida: 0, saldo: 12 },
-  { id: 3, nome: "Pedro", divida: 0, saldo: 15 },
-  { id: 4, nome: "Santiago", divida: 0, saldo: 15 },
+const pedidos = [
+  { id: 1, nome: "Caio", divida: 10, saldo: 0 },
+  { id: 2, nome: "Lucas", divida: 230, saldo: 12 },
+  { id: 3, nome: "Pedro", divida: 420, saldo: 15 },
+  { id: 4, nome: "Santiago", divida: 1230, saldo: 15 },
   { id: 5, nome: "Maria", divida: 0, saldo: 15 },
-]
+];
 
-const verificarIndividados = pedidosDaPizzaria.filter((verificar) => {
-  return verificar.divida > 0;
+const inadimplentes = [];
+const adimplentes = [];
+
+pedidos.forEach(percorrer => {
+  if (percorrer.divida > 0){
+    inadimplentes.push(percorrer);
+  }else {
+    adimplentes.push(percorrer);
+  }
 });
 
-if (verificarIndividados.length > 0){
-  console.log("Atenção! Clientes com dívidas pendentes!")
-
-  verificarIndividados.forEach((verificar) => {
-    console.log(`-- ${verificar.nome} está devendo R$ ${verificar.divida}`);
+if (inadimplentes.length > 0){
+  console.log("Clientes com dívidas pedentes:");
+  inadimplentes.forEach((verificar) => {
+    console.log(`--- ${verificar.nome} deve R$ ${verificar.divida}`);
   });
-}else{
-  console.log("Ótima notícias! Nenhum cliente está devendo.")
-}
+};
+
+if (adimplentes.length > 0){
+  console.log("Clientes com o pagamento em dia:");
+  adimplentes.forEach((verificar) => {
+    console.log(`--- ${verificar.nome} está com tudo pago! Saldo: ${verificar.saldo}`);
+  });
+};
 
