fcc420661c66a6e058187a5c2a4320eb521e7d6c
Jackson Porciúncula <jbrunops@outlook.com>
2025-08-05 10:21:11 -0300

    Erro usando o DOM em uma callback - nessa primeira revisão vou deixar passar e depois volto para aprofundamento

--- callback.js
+++ callback.js
@@ -1,13 +1,17 @@
-const mensagem = function() {
-  console.log("RESULTADO 1")
-}
+// const mensagem = function() {
+//   console.log("RESULTADO 1")
+// }
 
-setTimeout(mensagem, 3000);
+// setTimeout(mensagem, 3000);
 
-setTimeout(function(){
-  console.log("RESULTADO 2")
-}, 4000);
+// setTimeout(function(){
+//   console.log("RESULTADO 2")
+// }, 4000);
 
-setTimeout(() => {
-   console.log("RESULTADO 3")
-}, 6000);
+// setTimeout(() => {
+//    console.log("RESULTADO 3")
+// }, 6000);
+
+document.queryselector("#callback-btn").addEventListener('click', function(){
+  console.log("CLICOU! RESULTADO 1");
+});
