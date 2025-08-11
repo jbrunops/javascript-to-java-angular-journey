c6f9d09554c893fd585efa2f45bcb8e2875b88df
Jackson Porciúncula <jbrunops@outlook.com>
2025-08-11 17:06:50 -0300

    Começando no Typescript - configurando o primeiro ts-node

---
+++ typescript/fundamentos/aula1.js
@@ -0,0 +1,8 @@
+console.log("-- Estudando typescript --");
+var pizzeriaName = "La Notte";
+var dailyRevenue = 5500.00;
+var isOpen = true;
+console.log("Pizzaria: ".concat(pizzeriaName));
+console.log("Faturamento de hoje: R$: ".concat(dailyRevenue));
+console.log("Est\u00E1 aberto? ".concat(isOpen));
+pizzeriaName = 12345;
---
+++ typescript/fundamentos/aula1.ts
@@ -0,0 +1,12 @@
+console.log("-- Estudando typescript --");
+
+let pizzeriaName: string = "La Notte";
+let dailyRevenue: number = 5500.00;
+
+const isOpen: boolean = true;
+
+console.log(`Pizzaria: ${pizzeriaName}`);
+console.log(`Faturamento de hoje: R$: ${dailyRevenue}`);
+console.log(`Está aberto? ${isOpen}`);
+
+// pizzeriaName = 12345;
